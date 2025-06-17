f2ab1cb4544ef479f8a0109d5794707e06f03664
Dalin <dalin.ctg@gmail.com>
2025-06-17 17:56:16 +0700

    refactor(service-page): apply cagetory responsive

--- src/app/components/landing/IntroductionBlock.tsx
+++ src/app/components/landing/IntroductionBlock.tsx
@@ -5,15 +5,15 @@ import { TypingAnimation } from "@/app/components/TypingAnimation";
 export const IntroductionBlock = async () => {
   const t = await getTranslations("landing");
   return (
-    <div className="text-center mt-[50px] mb-6">
+    <div className="text-center mt-5 sm:mt-[50px] mb-6">
       <h1
-        className={`${fadeIn} flex justify-center items-center text-[32px] font-bold sm:text-[48px] mx-auto text-orange-500`}
+        className={`${fadeIn} flex justify-center items-center text-[24px] font-bold sm:text-[48px] mx-auto text-orange-500`}
       >
         <TypingAnimation />
         <span className="ml-1 mt-[-4px] h-[37px] w-[3px] animate-blink bg-[#CD202A] opacity-60"></span>
         <span className="ml-2 text-white">K157</span>
       </h1>
-      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-800 text-white">
+      <h1 className="text-xl sm:text-3xl md:text-4xl font-bold text-gray-800 text-white">
         {t.rich("main_title", {
           highlight: (chunks) => (
             <span className="text-orange-500">{chunks}</span>
